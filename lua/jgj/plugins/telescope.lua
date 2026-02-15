3e277fdcfe0dafd07b4fbe1d1a792571629f9e9f
Joel Gonzalez <joel.gonzalez.jimenezjgj@gmail.com>
2026-02-15 17:10:16 +0100

    Removed luarocks dependency

--- lua/jgj/plugins/telescope.lua
+++ lua/jgj/plugins/telescope.lua
@@ -1,22 +1,14 @@
 return {
 	"nvim-telescope/telescope.nvim",
-
-	branch = "0.1.x",
-
+    version = "*",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-		"nvim-tree/nvim-web-devicons",
-		"folke/todo-comments.nvim",
 	},
 
 	config = function()
 		local telescope = require("telescope")
 		local actions = require("telescope.actions")
-		local transform_mod = require("telescope.actions.mt").transform_mod
-		local trouble = require("trouble")
-		local trouble_telescope = require("trouble.sources.telescope")
-
 		telescope.setup({
 			defaults = {
 				path_display = { "smart" },
@@ -28,26 +20,30 @@ return {
 				},
 			},
 		})
-
-		-- better performance
-		telescope.load_extension("fzf")
-
-		-- set keymaps
+		--telescope.load_extension("fzf_native")
 		local builtin = require("telescope.builtin")
 		vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-		vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+		vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+		vim.keymap.set("n", "<leader>fl", builtin.live_grep, {})
+		vim.keymap.set("n", "<leader>D", function() builtin.diagnostics{bufnr = 0} end, {})
+		vim.keymap.set("n", "<leader>gD", builtin.diagnostics, {})
 		vim.keymap.set("n", "<C-p>", builtin.git_files, {})
-		vim.keymap.set("n", "<leader>fW", function()
+		vim.keymap.set("n", "<leader>fw", function()
 			local word = vim.fn.expand("<cword>")
 			builtin.grep_string({ search = word })
 		end)
-		vim.keymap.set("n", "<leader>fw", function()
+		vim.keymap.set("n", "<leader>fW", function()
 			local word = vim.fn.expand("<cWORD>")
 			builtin.grep_string({ search = word })
 		end)
 		vim.keymap.set("n", "<leader>fs", function()
 			builtin.grep_string({ search = vim.fn.input("Grep > ") })
 		end)
-		vim.keymap.set("n", "<leader>vh", builtin.help_tags, {})
+		vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+        vim.keymap.set("n", "gR", builtin.lsp_references, {})
+        vim.keymap.set("n", "gd", builtin.lsp_definitions, {})
+        vim.keymap.set("n", "gi", builtin.lsp_implementations, {})
+        vim.keymap.set("n", "gs", builtin.lsp_document_symbols, {})
+        vim.keymap.set("n", "gy", builtin.lsp_type_definitions, {})
 	end,
 }
